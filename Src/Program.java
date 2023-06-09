2e009815321bd718bbee3d3a5051d3f558d0c7f0
VitaliyNovickiy <vidmarev@mail.ru>
2023-06-09 11:22:21 +0300

    NewTaskDZ8

--- Src/Program.java
+++ Src/Program.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Program {
     public static void main (String[] args){
-        System.out.println();
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(11);
+        int attempts = 0;
+        int guess;
+
+        System.out.println("Вітаю! Я загадав число від 0 до 10. Спробуйте відгадати.");
+
+        while (true) {
+            System.out.print("Введіть вашу догадку: ");
+            guess = input.nextInt();
+              attempts++;
+
+            if (guess == secretNumber) {
+                System.out.println("Вітаю! Ви відгадали число " + secretNumber + " з " + attempts + " спроби.");
+                break;
+            } else if (attempts >= 3) {
+                System.out.println("Ви не вгадали. Правильне число було " + secretNumber + ".");
+                break;
+            } else {
+                System.out.println("Неправильно! Спробуйте ще раз.");
+            }
+        }
     }
 }
